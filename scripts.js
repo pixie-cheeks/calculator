c91de0619c57d857640b274ec3e938e6a90aa46f
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2023-08-06 21:23:55 +0530

    Add some styles

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let firstNum;
+let secondNum;
+let operator;
+
 function add(addendOne, addendTwo) {
 	return addendOne + addendTwo;
 }
@@ -12,4 +16,19 @@ function multiply(factorOne, factorTwo) {
 
 function divide(dividend, divisor) {
 	return dividend / divisor;
+}
+
+function operate(operator, firstNum, secondNum) {
+	switch (operator) {
+		case '+':
+			return add(firstNum, secondNum);
+		case '-':
+			return subtract(firstNum, secondNum);
+		case '*':
+			return multiply(firstNum, secondNum);
+		case '/':
+			return divide(firstNum, secondNum);
+		default:
+			return 'Error from operate()'
+	}
 }
