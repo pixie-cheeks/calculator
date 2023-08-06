d8d3737a766820bf777c4c86981c9f7c386f5e66
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2023-08-06 17:23:40 +0530

    Initialize the project with boilerplate

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function add(addendOne, addendTwo) {
+	return addendOne + addendTwo;
+}
+
+function subtract(minuend, subtrahend) {
+	return minuend - subtrahend;
+}
+
+function multiply(factorOne, factorTwo) {
+	return factorOne * factorTwo;
+}
+
+function divide(dividend, divisor) {
+	return dividend / divisor;
+}
